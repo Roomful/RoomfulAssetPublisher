f6f1b7cc9d137400bc502fea75bea1004933ce3b
Vitalii Kolomiiets <vitalykolomiets@gmail.com>
2017-12-08 19:05:51 +0200

    colliders meta data done

---
+++ AssetBundlesPlugin/Assets/Plugins/Roomful/Scripts/Models/Templates/ColliderMetaData.cs
@@ -0,0 +1,93 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace RF.AssetWizzard {
+
+    [System.Serializable]
+    public class ColliderMetaData {
+        public Vector3 Position = Vector3.zero;
+        public Vector3 Rotation = Vector3.zero;
+        public Vector3 Scale = Vector3.one;
+
+        public Vector3 Center = Vector3.zero;
+        public Vector3 Size = Vector3.one;
+
+        public bool MarkedAsDisable = false;
+
+        public ColliderMetaData() {
+            
+        }
+
+        public ColliderMetaData(JSONData data) {
+            Parse(data);
+        }
+
+        public Dictionary<string, object> ToDictionary() {
+            Dictionary<string, object> data = new Dictionary<string, object>();
+
+            Dictionary<string, object> pos = new Dictionary<string, object>();
+            pos.Add("x", Position.x);
+            pos.Add("y", Position.y);
+            pos.Add("z", Position.z);
+
+            Dictionary<string, object> rot = new Dictionary<string, object>();
+            rot.Add("x", Rotation.x);
+            rot.Add("y", Rotation.y);
+            rot.Add("z", Rotation.z);
+
+            Dictionary<string, object> sca = new Dictionary<string, object>();
+            sca.Add("x", Scale.x);
+            sca.Add("y", Scale.y);
+            sca.Add("z", Scale.z);
+
+            Dictionary<string, object> cen = new Dictionary<string, object>();
+            cen.Add("x", Center.x);
+            cen.Add("y", Center.y);
+            cen.Add("z", Center.z);
+
+            Dictionary<string, object> siz = new Dictionary<string, object>();
+            siz.Add("x", Size.x);
+            siz.Add("y", Size.y);
+            siz.Add("z", Size.z);
+
+            data.Add("position", pos);
+            data.Add("rotation", rot);
+            data.Add("scale", sca);
+            data.Add("center", cen);
+            data.Add("size", siz);
+            data.Add("disabled", MarkedAsDisable);
+
+            return data;
+        }
+
+        private void Parse(JSONData data) {
+            JSONData posData = new JSONData(data.GetValue<Dictionary<string, object>>("position"));
+            Position.x = posData.GetValue<float>("x");
+            Position.y = posData.GetValue<float>("y");
+            Position.z = posData.GetValue<float>("z");
+
+            JSONData rotData = new JSONData(data.GetValue<Dictionary<string, object>>("rotation"));
+            Rotation.x = rotData.GetValue<float>("x");
+            Rotation.y = rotData.GetValue<float>("y");
+            Rotation.z = rotData.GetValue<float>("z");
+
+            JSONData scaData = new JSONData(data.GetValue<Dictionary<string, object>>("scale"));
+            Scale.x = scaData.GetValue<float>("x");
+            Scale.y = scaData.GetValue<float>("y");
+            Scale.z = scaData.GetValue<float>("z");
+
+            JSONData cenData = new JSONData(data.GetValue<Dictionary<string, object>>("center"));
+            Center.x = cenData.GetValue<float>("x");
+            Center.y = cenData.GetValue<float>("y");
+            Center.z = cenData.GetValue<float>("z");
+
+            JSONData sizData = new JSONData(data.GetValue<Dictionary<string, object>>("size"));
+            Size.x = sizData.GetValue<float>("x");
+            Size.y = sizData.GetValue<float>("y");
+            Size.z = sizData.GetValue<float>("z");
+
+            MarkedAsDisable = data.GetValue<bool>("disabled");
+        }
+    }
+}
--- AssetBundlesPlugin/Assets/Plugins/Roomful/Scripts/Models/Templates/StylePanelMetadata.cs
+++ AssetBundlesPlugin/Assets/Plugins/Roomful/Scripts/Models/Templates/StylePanelMetadata.cs
@@ -6,22 +6,23 @@ using UnityEngine;
 namespace RF.AssetWizzard
 {
     [System.Serializable]
-    public class StylePanelMetadata
-    {
+    public class StylePanelMetadata {
 
         public string Name = string.Empty;
         public Vector3 Size = Vector3.zero;
-
+        public List<ColliderMetaData> CollidersMetaData = new List<ColliderMetaData>();
+        
         public StylePanelMetadata(StylePanel panel) {
             Name = panel.name;
             Size = panel.Bounds.size;
+
+            CreateCollidersMetaData(panel);
         }
 
         public StylePanelMetadata(JSONData panelInfo) {
             ParseTemplate(panelInfo);
         }
-
-
+        
         public Dictionary<string, object> ToDictionary() {
 
             Dictionary<string, object> data = new Dictionary<string, object>();
@@ -34,18 +35,56 @@ namespace RF.AssetWizzard
 
             data.Add("size", size);
 
+            List<object> collList = new List<object>();
+
+            foreach (ColliderMetaData cmd in CollidersMetaData) {
+                collList.Add(cmd.ToDictionary());
+            }
+
+            data.Add("colliders", collList);
+            
             return data;
         }
-
-
+        
         private void ParseTemplate(JSONData panelInfo) {
-
             Name = panelInfo.GetValue<string>("name");
             JSONData MobileGeometrySize = new JSONData(panelInfo.GetValue<Dictionary<string, object>>("size"));
 
             Size.x = MobileGeometrySize.GetValue<float>("x");
             Size.y = MobileGeometrySize.GetValue<float>("y");
             Size.z = MobileGeometrySize.GetValue<float>("z");
+
+            if (panelInfo.HasValue("colliders")) {
+                List<object> collList = panelInfo.GetValue<List<object>>("colliders");
+
+                foreach (object coll in collList) {
+                    JSONData parsedColl = new JSONData(coll);
+
+                    CollidersMetaData.Add(new ColliderMetaData(parsedColl));
+                }
+            }
+        }
+
+        private void CreateCollidersMetaData(StylePanel panel) {
+            BoxCollider[] colliders = panel.GetComponentsInChildren<BoxCollider>();
+
+            foreach (BoxCollider bc in colliders) {
+                ColliderMetaData newMetaData = new ColliderMetaData();
+
+                Transform cachedParent = bc.transform.parent;
+                bc.transform.SetParent(panel.transform);
+
+                newMetaData.Position = bc.transform.localPosition;
+                newMetaData.Rotation = bc.transform.localRotation.eulerAngles;
+                newMetaData.Scale = bc.transform.localScale;
+                newMetaData.Center = bc.center;
+                newMetaData.Size = bc.size;
+                newMetaData.MarkedAsDisable = bc.GetComponent<RF.AssetBundles.Serialization.SerializedDisabledAreaMarker>() != null;
+                    
+                CollidersMetaData.Add(newMetaData);
+
+                bc.transform.SetParent(cachedParent);
+            }
         }
     }
 }
