5b9d3a08c4cd8deaf304d6e7856efa44d2f77a7e
StanislavOsipov <lacost.st@gmail.com>
2018-07-25 16:39:20 +0300

    added the prot display

--- AssetBundlesPlugin/Assets/Plugins/Roomful/Editor/Windows/Wizzard/WizzardWindow.cs
+++ AssetBundlesPlugin/Assets/Plugins/Roomful/Editor/Windows/Wizzard/WizzardWindow.cs
@@ -43,6 +43,8 @@ namespace RF.AssetWizzard.Editor {
 
 		void OnGUI() {
 
+            titleContent = new GUIContent("Roomful Plugin - port " + AssetBundlesSettings.PORT);
+
 			GUI.changed = false;
 			EditorGUIUtility.labelWidth = 200f;
 		
