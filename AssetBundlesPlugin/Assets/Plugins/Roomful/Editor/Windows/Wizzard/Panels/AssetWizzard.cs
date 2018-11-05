abe99a96e273fc97957b358136ab7b0cf1ff9a6b
Igor Anokhin <igor.a.anokhin@gmail.com>
2018-11-05 15:40:57 +0200

    transparent screenshot

--- AssetBundlesPlugin/Assets/Plugins/Roomful/Editor/Windows/Wizzard/Panels/AssetWizzard.cs
+++ AssetBundlesPlugin/Assets/Plugins/Roomful/Editor/Windows/Wizzard/Panels/AssetWizzard.cs
@@ -73,12 +73,12 @@ namespace RF.AssetWizzard.Editor
                     Download();
                 }
             } 
-           // if (Asset is PropAsset && Asset.GetIcon() == null) {
+            if (Asset is PropAsset) {
                 bool createIcon = GUI.Button(buttonRect4, "Create icon");
                 if (createIcon) {
                     CreateIcon();
                 }
-           // }
+            }
 
             bool create = GUI.Button(buttonRect3, "Create New");
             if (create) {
@@ -90,29 +90,17 @@ namespace RF.AssetWizzard.Editor
             GUILayout.EndHorizontal();
         }
 
-        private void CreateIcon() { 
+        private void CreateIcon() {
             if (!(Asset is PropAsset)) {
                 return;
             }
             var savedLayer = Asset.gameObject.layer;
             const int screenShotLayer = 31;
-            foreach (Transform trans in Asset.gameObject.GetComponentsInChildren<Transform>(true)) {
-                trans.gameObject.layer = screenShotLayer;
-            }
-            var cameraHolder = new GameObject();
+            SetLayerForAsset(screenShotLayer);
             var enviroment = EditorSceneManager.GetActiveScene().GetComponentInScene<Environment>();
+            var cameraHolder = new GameObject();
             cameraHolder.transform.SetParent(enviroment.gameObject.transform);
             var camera = cameraHolder.AddComponent<Camera>();
-            camera.cullingMask = (1 << screenShotLayer);
-            camera.clearFlags = CameraClearFlags.SolidColor;
-            camera.backgroundColor = Color.green;
-            RenderTexture rt = new RenderTexture(256, 256, 32);
-            Texture2D icon = new Texture2D(rt.width, rt.height, TextureFormat.ARGB32, false);
-            var cameraPosition = camera.transform.position;
-            var cameraRotation = camera.transform.rotation;
-            var savedClearFlag = camera.clearFlags;
-            var savedRenderTexture = camera.targetTexture;
-            var savedActiveState = camera.enabled;
             PropAsset prop = Asset as PropAsset;
             if (prop.Template.Placing == Placing.Floor) {
                 SetupCameraForFloorProp(prop.Template.Size, prop.gameObject.transform.position, camera);
@@ -120,24 +108,61 @@ namespace RF.AssetWizzard.Editor
             else {
                 SetupCameraForWallProp(prop.Template.Size, prop.gameObject.transform.position, camera);
             }
-            camera.targetTexture = rt;
-            camera.Render();
+            Texture2D whiteScreenshot = MakeScreenshotWithBackground(camera, Color.white, screenShotLayer);
+            Texture2D blackScreenshot = MakeScreenshotWithBackground(camera, Color.black, screenShotLayer);
 
-            camera.targetTexture = savedRenderTexture;
-            camera.clearFlags = savedClearFlag;
-            camera.enabled = savedActiveState;
-            RenderTexture.active = rt;
-            icon.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
-            icon.Apply();
-            RenderTexture.active = null;
-            (Asset as PropAsset).Icon = icon;
-            
-            foreach (Transform trans in Asset.gameObject.GetComponentsInChildren<Transform>(true)) {
-                trans.gameObject.layer = savedLayer;
-            }
+            Texture2D resultIcon = MakeTextureFromIntersection(whiteScreenshot, blackScreenshot);
+            prop.Icon = resultIcon;
+
+            SetLayerForAsset(savedLayer);
             GameObject.DestroyImmediate(cameraHolder);
         }
 
+        private void SetLayerForAsset(int savedLayer) {
+            foreach (Transform trans in Asset.gameObject.GetComponentsInChildren<Transform>(true)) {
+                trans.gameObject.layer = savedLayer;
+            }
+        }
+
+        private Texture2D MakeScreenshotWithBackground(Camera camera, Color background, int layer) {
+
+            camera.cullingMask = (1 << layer);
+            camera.clearFlags = CameraClearFlags.SolidColor;
+            camera.backgroundColor = background;
+
+            RenderTexture rt = new RenderTexture(256, 256, 32);
+            Texture2D screenshot = new Texture2D(rt.width, rt.height, TextureFormat.RGBA32, false);
+            camera.targetTexture = rt;
+            camera.Render();
+
+            RenderTexture.active = rt;
+            screenshot.ReadPixels(new Rect(0, 0, rt.width, rt.height), 0, 0);
+            screenshot.Apply();
+            RenderTexture.active = null;
+            rt.DiscardContents();
+            return screenshot;
+        }
+
+        private Texture2D MakeTextureFromIntersection(Texture2D left, Texture2D right) {
+            if (left.width != right.width || left.height != right.height) {
+                throw new ArgumentException("Textures dimentions must be equal");
+            }
+            Texture2D resultIcon = new Texture2D(left.width, left.height, TextureFormat.RGBA32, false);
+            var leftColors = left.GetPixels32();
+            var rightColors = right.GetPixels32();
+            Color32[] resultColors = new Color32[leftColors.Length];
+            for (var i = resultColors.Length - 1; i >= 0; i--) {
+                if (leftColors[i].Equals(rightColors[i])) {
+                    resultColors[i] = leftColors[i];
+                }
+                else {
+                    resultColors[i] = Color.clear;
+                }
+            }
+            resultIcon.SetPixels32(resultColors);
+            resultIcon.Apply();
+            return resultIcon;
+        }
 
         public static void SetupCameraForFloorProp(Vector3 size, Vector3 position, Camera camera) {
             float halfX = size.x /2 ;
