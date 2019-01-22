818c1493d4c46f0382c8f6ab8b13659b84ae585a
Konstantin Koretsky <frostinglg@gmail.com>
2019-01-22 17:30:34 +0200

    Added animation marker for animation hitboxes

---
+++ AssetBundlesPlugin/Assets/Plugins/Roomful/Scripts/Models/AnimationMarker.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using UnityEngine;
+
+namespace RF.AssetBundles.Serialization {
+    
+    [Serializable]
+    [RequireComponent(typeof(Collider))]
+    public class AnimationMarker : MonoBehaviour {
+
+        public BoxCollider Collider;
+        public string Key;
+    }
+}
