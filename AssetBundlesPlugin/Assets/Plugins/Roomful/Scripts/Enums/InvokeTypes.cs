f4aeb23701233231bcca7f8e8f33ac133a34a88c
Konstantin Koretsky <frostinglg@gmail.com>
2020-01-10 17:40:12 +0200

    feat: Add type Avatar for props

--- AssetBundlesPlugin/Assets/Plugins/Roomful/Scripts/Enums/InvokeTypes.cs
+++ AssetBundlesPlugin/Assets/Plugins/Roomful/Scripts/Enums/InvokeTypes.cs
@@ -1,10 +1,14 @@
-﻿namespace RF.AssetWizzard {
+﻿// Copyright Roomful 2013-2020. All rights reserved.
+
+namespace RF.AssetWizzard {
+
 	public enum InvokeTypes {
 		Default = 1,
 		Executable = 2,
 		FileCabinet = 3,
 		Bookshelf = 4,
 		NoAction = 5,
-		Container = 6
+		Container = 6,
+		Avatar = 7
 	}
 }
