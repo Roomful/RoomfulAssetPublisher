833feba4e31a7b30253c5f8d6e10912c1ea45d1f
Alexey Yaremenko <alex@stansassets.com>
2020-07-07 19:57:01 +0300

    video streaming prop scripts

---
+++ AssetBundlesPlugin/Assets/Plugins/Roomful/Scripts/Streaming/Scripts/IVideoTexturePlugin.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public interface IVideoTexturePlugin
+{
+    void Init();
+
+    Texture2D PresenterTex { get; }
+    Texture2D ShareScreenTex { get; }
+
+    bool IsPresenterTextureReady { get; }
+    bool IsShareScreenTextureReady { get; }
+}
